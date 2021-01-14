045cc27939b0dc71bad8542ac453c8b2c00cf60e
Marcus Alexandru <alexandru.marcus@webwavers.net>
2021-01-14 15:59:56 +0200

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+// 
+// @Alexandru Marcus
+// 
+
 var express = require("express");
 var port = process.env.PORT || 3002;
+var exphbs = require('express-handlebars');
+var path = require('path');
+var Routing = require('./App/router/index');
+var reload = require('reload');
+
 
 var app = express();
+let routeInfo = new Routing.JsonRoute(app, {
+    routesPath: "./App/router",
+    controllersPath: "./App/router/controllers",
+    defaultAction: "index",
+    cors: true,
+    displayRoute: false,
+    "processdir": __dirname
+}).start();
+app.set('views', path.join(__dirname + '/App/views'));
 
-app.get('/', (rq, rs) => {
-    rs.send(JSON.stringify({
-        Hello: 'World'
-    }));
+app.engine('handlebars', exphbs());
+app.set("view engine, 'handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var router = express.Router();
+router.get('/express/', function (req, res) {
+    res.send(' this is a standard routing ');
 });
+app.use('/', router);
 
 app.listen(port, () => {
     console.log('App started...');
 })
+
+reload(app);
